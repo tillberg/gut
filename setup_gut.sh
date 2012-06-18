970c2578151913dc0ca7138c814e3522d2dfbf98
Dan Tillberg <dan@tillberg.us>
2012-06-18 11:16:32 -0400

    fixed gut install path

--- setup_gut.sh
+++ setup_gut.sh
@@ -1,6 +1,7 @@
 git clone git://github.com/tillberg/git.git
+# sudo apt-get install curl libcurl4-openssl-dev libexpat1-dev gettext
 cd git
 make configure
-./configure
+./configure --prefix=/usr
 make
 sudo make install
