27a5f63df1a3026fc23a79d3df437383b073c3b5
Dan Tillberg <dan@tillberg.us>
2012-06-18 23:02:20 -0400

    changed pip to sudo

--- setup_gut.sh
+++ setup_gut.sh
@@ -1,5 +1,5 @@
 npm install
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
 git clone git://github.com/tillberg/git.git
 # sudo apt-get install curl libcurl4-openssl-dev libexpat1-dev gettext
 cd git
