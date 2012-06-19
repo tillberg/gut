40fbc42a4fde6d7f679f885ac04ccad79b791855
Dan Tillberg <dan@tillberg.us>
2012-06-18 23:01:00 -0400

    switched to watchdog in python for fs monitoring

---
+++ lib/monitor.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from watchdog.events import EVENT_TYPE_MOVED, EVENT_TYPE_DELETED, EVENT_TYPE_CREATED, EVENT_TYPE_MODIFIED
+
+class FSEventHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        """Catch-all event handler.
+
+        :param event:
+            The event object representing the file system event.
+        :type event:
+            :class:`FileSystemEvent`
+        """
+        if not event.is_directory and not '/.gut/' in event.src_path:
+            if event.event_type == EVENT_TYPE_MOVED:
+                print '%s %s' % (EVENT_TYPE_DELETED, event.src_path)
+                print '%s %s' % (EVENT_TYPE_CREATED, event.dest_path)
+            else:
+                print '%s %s' % (event.event_type, event.src_path)
+            sys.stdout.flush()
+
+if __name__ == "__main__":
+    observer = Observer()
+    observer.schedule(FSEventHandler(), path=sys.argv[1], recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
--- setup_gut.sh
+++ setup_gut.sh
@@ -1,3 +1,5 @@
+npm install
+pip install -r requirements.txt
 git clone git://github.com/tillberg/git.git
 # sudo apt-get install curl libcurl4-openssl-dev libexpat1-dev gettext
 cd git
