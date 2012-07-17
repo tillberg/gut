a0b534a0cdcb8c3e7b6ed9fec23fd40919b59391
Dan Tillberg <dan@tillberg.us>
2012-07-17 16:21:45 -0700

    changed to be more forthcoming about adding lots of files at once

--- setup_gut.sh
+++ setup_gut.sh
@@ -2,7 +2,7 @@ npm install
 sudo pip install -r requirements.txt
 sudo sysctl fs.inotify.max_user_watches=1048576
 git clone git://github.com/tillberg/git.git
-# sudo apt-get install curl libcurl4-openssl-dev libexpat1-dev gettext
+# sudo apt-get install curl libcurl4-openssl-dev libexpat1-dev gettext autoconf
 cd git
 make configure
 ./configure --prefix=/usr
