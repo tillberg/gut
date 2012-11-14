0ee3add58b264798e6960fd621c3ce1a0d3e12f1
Dan Tillberg <tillberg@tracelytics.com>
2012-11-14 17:22:07 +0000

    Tweaked the setup script

--- setup_gut.sh
+++ setup_gut.sh
@@ -1,9 +1,8 @@
 npm install
-sudo apt-get install gettext
-sudo pip install -r requirements.txt
+sudo apt-get install gettext libyaml-dev curl libcurl4-openssl-dev libexpat1-dev gettext autoconf
 sudo sysctl fs.inotify.max_user_watches=1048576
+pip install -r requirements.txt
 git clone git://github.com/tillberg/git.git
-# sudo apt-get install curl libcurl4-openssl-dev libexpat1-dev gettext autoconf
 cd git
 make configure
 ./configure --prefix=/usr
@@ -11,6 +10,5 @@ make
 sudo make install
 cd ..
 git clone git://github.com/tillberg/watchdog.git
-sudo aptitude install libyaml-dev
 cd watchdog
 sudo python setup.py install
