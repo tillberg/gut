e44e6490c697ccabd7ea3bfe08c8745a1d0de1f7
Dan Tillberg <dan@tillberg.us>
2014-02-20 16:37:37 -0500

    bring-your-own nodejs, and install watchdog via pip now that it's fixed

--- setup_gut.sh
+++ setup_gut.sh
@@ -7,7 +7,7 @@ npm install
 brew install libyaml
 
 # These next two commands fail on OSX, but that's okay:
-sudo apt-get install gettext libyaml-dev curl libcurl4-openssl-dev libexpat1-dev autoconf python-pip python-dev nodejs
+sudo apt-get install gettext libyaml-dev curl libcurl4-openssl-dev libexpat1-dev autoconf python-pip python-dev
 sudo sysctl fs.inotify.max_user_watches=1048576
 
 
@@ -20,9 +20,6 @@ git clone git://github.com/tillberg/git.git
 cd git
 make configure
 ./configure --prefix=/usr
-make
+make -j 16
 sudo make install
 cd ..
-git clone git://github.com/tillberg/watchdog.git
-cd watchdog
-sudo python setup.py install
