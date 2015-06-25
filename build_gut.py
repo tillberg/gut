a681239f7053d0d3cb6cf4858942c89adafa253b
Sky <dan@tillberg.us>
2015-06-25 11:35:10 -0400

    Rename setup_gut to build_gut

--- build_gut.py
+++ build_gut.py
@@ -10,18 +10,20 @@ from sys import platform as _platform
 is_osx = _platform == 'darwin'
 
 GIT_VERSION = 'v2.4.4'
+dist_path = os.path.abspath('gut-dist')
 
 def system(s, cwd=None):
     for line in s.strip().split('\n'):
         print('> %s' % (line,))
         subprocess.call(line, cwd=cwd, shell=True)
 
-system("npm install")
 if is_osx:
     system('brew install libyaml')
 else:
     system('sudo apt-get install gettext libyaml-dev curl libcurl4-openssl-dev libexpat1-dev autoconf') #python-pip python-dev
     system('sudo sysctl fs.inotify.max_user_watches=1048576')
+
+system("npm install")
 
 #echo 'kern.maxfiles=20480' | sudo tee -a /etc/sysctl.conf
 #echo -e 'limit maxfiles 8192 20480\nlimit maxproc 1000 2000' | sudo tee -a /etc/launchd.conf
@@ -34,14 +36,15 @@ else:
     system('git fetch', 'gut/')
 system('git reset --hard %s' % (GIT_VERSION,), 'gut/')
 system('git clean -fd', 'gut/')
+system('make prefix=%s clean' % (dist_path,), 'gut/')
 
 def rename_git_to_gut(s):
     return s.replace('GIT', 'GUT').replace('Git', 'Gut').replace('git', 'gut')
 
 for root, dirs, files in os.walk('gut/'):
     for filename in files:
-        if filename == '.gitignore':
-            # don't touch .gitignores
+        if filename.startswith('.git'):
+            # don't touch .gitignores or .gitattributes files
             continue
         orig_path = os.path.join(root, filename)
         path = os.path.join(root, rename_git_to_gut(filename))
@@ -76,7 +79,6 @@ for root, dirs, files in os.walk('gut/'):
             shutil.move(orig_path, path)
         dirs.append(folder)
 
-dist_path = os.path.abspath('gut-dist')
 system('''
 make prefix=%s -j %s
 make prefix=%s install
