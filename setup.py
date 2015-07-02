1472087702d7b1669a0f1ad71c5e96b7b5edb3c6
Dan Tillberg <dan@tillberg.us>
2015-07-02 14:08:53 -0400

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "gut",
-    version = "0.1.1.dev3",
+    version = "0.1.1",
     author = "Dan Tillberg",
     author_email = "dan@tillberg.us",
     description = ("Realtime bidirectional folder synchronization via modified git"),
