2a5b14a2012a4257552665b1893537c0ac925619
Dan Tillberg <dan@tillberg.us>
2015-07-22 19:08:48 -0400

    Started porting EnsureBuild

--- main.go
+++ main.go
@@ -24,8 +24,35 @@ var OptsSync struct {
     } `positional-args:"yes" required:"yes"`
 }
 
+func EnsureBuild(ctx *SyncContext) (bool, error) {
+    status := ctx.Logger()
+    desiredGitVersion := GitVersion
+    if ctx.IsWindows() {
+        desiredGitVersion = GitWinVersion
+    }
+    status.Printf("@(dim:We want git version) %s\n", desiredGitVersion)
+    // if not gut_cmd.exe_path(context).exists() or desired_git_version.lstrip('v') not in gut_cmd.get_version(context):
+    //     status.out('(@dim)Need to build gut on ' + context._name_ansi + '(@dim).(@r)\n')
+    //     gut_build.ensure_gut_folders(context)
+    //     yield from gut_build.prepare(context)
+    //     if context != plumbum.local:
+    //         # If we're building remotely, rsync the prepared source to the remote host
+    //         build_path = config.GUT_SRC_TMP_PATH
+    //         yield from util.rsync(plumbum.local, config.GUT_SRC_PATH, context, build_path, excludes=['.git', 't'])
+    //     else:
+    //         build_path = config.GUT_WIN_SRC_PATH if context._is_windows else config.GUT_SRC_PATH
+    //     yield from gut_build.build(context, build_path)
+    //     status.out('(@dim)Cleaning up...(@r)')
+    //     yield from gut_build.unprepare(context)
+    //     if context != plumbum.local:
+    //         context['rm']['-r', context.path(config.GUT_SRC_TMP_PATH)]()
+    //     status.out('(@dim) done.(@r)\n')
+    //     return True
+    return false, nil
+}
+
 func doSession(ctx *SyncContext, done chan bool) {
-    err := ctx.RunShell("echo -n 'working... '; sleep 0.1; echo done.") //"/usr/bin/whoami"
+    err := ctx.RunShell("echo -n 'working... '; sleep 0.01; hostname")
     if err != nil {
         log.Fatalf("unable to connect: %s", err)
     }
@@ -33,49 +60,60 @@ func doSession(ctx *SyncContext, done chan bool) {
 }
 
 func main() {
+    log.EnableMultilineMode()
+    log.EnableColorTemplate()
+    log.AddAnsiColorCode("host", 33)
+    log.AddAnsiColorCode("path", 36)
+    log.AddAnsiColorCode("error", 31)
+    log.AddAnsiColorCode("commit", 32)
+    status := log.New(os.Stderr, "", 0)
     var args []string = os.Args[1:]
     if len(args) == 0 {
-        log.Fatalln("You must specify a gut-command, e.g. `gut sync ...`")
+        status.Fatalln("You must specify a gut-command, e.g. `gut sync ...`")
     }
     var cmd = args[0]
     args = args[1:]
     if IsGitCommand(cmd) {
         var gutExe = PathInUserHome(GutExePath)
         syscall.Exec(gutExe, append([]string { gutExe }, args...), os.Environ())
-        log.Fatalf("Failed to exec %s", gutExe)
+        status.Fatalf("Failed to exec %s", gutExe)
     }
     var argsRemaining, err = flags.ParseArgs(&OptsCommon, args)
-    if err != nil { log.Fatal(err) }
+    if err != nil { status.Fatal(err) }
     // fmt.Printf("color: %s\n", OptsCommon.NoColor)
     if OptsCommon.Version {
-        log.Print("gut-sync version XXXXX")
+        status.Print("gut-sync version XXXXX")
         os.Exit(0)
     }
     if cmd == "build" {
-        // var local = NewSyncContext()
-        // if !EnsureBuild(local) {
-        //     log.Printf("(@dim)gut " + GitVersion + "(@dim) has already been built.\n")
-        // }
+        var local = NewSyncContext()
+        local.Connect()
+        didSomething, err := EnsureBuild(local)
+        if err != nil { status.Fatal(err) }
+        if !didSomething {
+            status.Printf("@(dim:gut) " + GitVersion + " @(dim:has already been built.)\n")
+        }
     } else if cmd == "sync" {
         var _, err = flags.ParseArgs(&OptsSync, argsRemaining)
-        if err != nil { log.Fatal(err) }
+        if err != nil { status.Fatal(err) }
 
         local := NewSyncContext()
         err = local.ParseSyncPath(OptsSync.Positional.LocalPath)
-        if err != nil { log.Fatalln(err) }
+        if err != nil { status.Fatalln(err) }
         remote := NewSyncContext()
         err = remote.ParseSyncPath(OptsSync.Positional.RemotePath)
-        if err != nil { log.Fatalln(err) }
+        if err != nil { status.Fatalln(err) }
         fmt.Printf("local: %s\n", local)
         fmt.Printf("remote: %s\n", remote)
 
-        log.EnableMultilineMode()
         done := make(chan bool)
-        num := 10
+        num := 100
         for i := 0; i < num; i++ {
             go doSession(remote, done)
+            go doSession(local, done)
         }
         for i := 0; i < num; i++ {
+            <-done
             <-done
         }
     }
--- sync_context.go
+++ sync_context.go
@@ -3,8 +3,10 @@ package main
 import (
     "errors"
     "fmt"
+    "os"
     "regexp"
     "github.com/tillberg/bismuth"
+    "github.com/tillberg/ansi-log"
 )
 
 type SyncContext struct {
@@ -13,7 +15,7 @@ type SyncContext struct {
     syncPath string
 }
 
-func NewSyncContext () *SyncContext {
+func NewSyncContext() *SyncContext {
     ctx := &SyncContext{}
     ctx.Init()
     return ctx
@@ -41,4 +43,16 @@ func (ctx *SyncContext) String() string {
         return fmt.Sprintf("{SyncContext %s@%s:%s}", ctx.Username(), ctx.Hostname(), ctx.syncPath)
     }
     return fmt.Sprintf("{SyncContext local %s}", ctx.syncPath)
+}
+
+func (ctx *SyncContext) nameAnsi() string {
+    hostname := ctx.Hostname()
+    if hostname == "" { hostname = "localhost" }
+    return fmt.Sprintf("@(host:%s)", hostname)
+}
+
+func (ctx *SyncContext) Logger() *log.Logger {
+    logger := log.New(os.Stderr, fmt.Sprintf("@(dim)[%s] ", ctx.nameAnsi()), 0)
+    logger.EnableColorTemplate()
+    return logger
 }
